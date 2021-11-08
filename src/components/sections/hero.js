02a00d9870007596d702c058f6ccbe05b8d7770b
AhnafAhamed <ahnafahamed4@gmail.com>
2021-11-08 09:20:28 +0530

    edit hero and about section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,28 +59,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Ahnaf Ahamed.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a front-end engineer specializing in building (and occasionally designing) exceptional
+        digital experiences. Currently, I’m a part of the front-end team at{' '}
+        <a href="https://macroactive.com/" target="_blank" rel="noreferrer">
+          MacroActive
+        </a>{' '}
+        building UI components using vue.js
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="mailto:ahnafahamed4@gmail.com" target="_blank" rel="noreferrer">
+      Email me
     </a>
   );
 
